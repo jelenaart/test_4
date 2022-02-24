9f1fe965097f360a250103999771abe62848ede5
ADMIN <ADMIN@DESKTOP-87R953O>
2022-02-24 23:09:34 +0200

    Добавьте файлы проекта.

---
+++ test_4/Ulesanne 1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        var lause = "owo owo";//lause, mis on vaja testida - kas seal on palindromid
+        lause = lause.Replace(" ", "").ToLower();
+        Console.WriteLine(lause);
+        var t = new string(lause.Reverse().ToArray());
+        Console.WriteLine(t == lause ? "See on palindrom" : "See ei ole palindrom");
+        Console.ReadLine();
+        }
+    }
+}
